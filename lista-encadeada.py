c0e00309a072ded07d7b18ddd734f1b3f74fb14c
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2023-08-28 11:37:01 -0300

    Update lista-encadeada.py

--- lista-encadeada.py
+++ lista-encadeada.py
@@ -31,6 +31,9 @@ class LinkedList:
     def insertEnd(self,data):
         node = Node(data)
         node_aux = self.head
+        if self.length == 0:
+            self.insertBeginning(data)
+            return
         while node_aux.next != None:
             node_aux = node_aux.next
         node_aux.next = node
@@ -90,21 +93,35 @@ class LinkedList:
         
         print(node.data)
     
-    
-    
+    def insertInOrder(self, lista, data):
+        if lista.length == 0:
+            lista.insertBeginning(data)
+        current = lista.head
+        while current.next != None:
+            if current.next.data > data:
+                break
+            current = current.next
+        newNode = Node(data)
+        if current.next == None:
+            current.next = newNode
+        else:
+            newNode.next = current.next
+            current.next = newNode
 
 lista_encadeada = LinkedList()
-lista_encadeada.insertBeginning('levy')
-lista_encadeada.insertBeginning('jorge')
-lista_encadeada.insertBeginning('careca')
-lista_encadeada.insertBeginning('jon')
-lista_encadeada.insertEnd('barros')
-lista_encadeada.insertEnd('barros')
-lista_encadeada.insertPosisition(3,5)
+# lista_encadeada.insertBeginning('1')
+# lista_encadeada.insertBeginning('2')
+# lista_encadeada.insertBeginning('3')
+# lista_encadeada.insertBeginning('4')
+lista_encadeada.insertEnd(1)
+lista_encadeada.insertEnd(2)
+lista_encadeada.insertEnd(3)
+lista_encadeada.insertEnd(4)
+lista_encadeada.insertInOrder(lista_encadeada, 2)
+# lista_encadeada.insertPosisition(3,5)
 lista_encadeada.printList()
-print('\n')
-print(lista_encadeada.fullForce(5))
-print(lista_encadeada.fastslow(5))
-print('\n')
-print('\n')
-lista_encadeada.printarinverso()
+
+# print(lista_encadeada.fullForce(5))
+# print(lista_encadeada.fastslow(5))
+
+# lista_encadeada.printarinverso()
